b2a402b48f85fa90309ea8dc8cfaf0d972cf43b3
Darya Matrosova <mostovichdi@gmail.com>
2025-10-12 13:45:57 +0300

    make data file, make fixture for sauce and filling

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,6 +1,8 @@
+from praktikum import ingredient
 from praktikum.burger import Burger
 from unittest.mock import Mock 
 from praktikum.ingredient_types import *
+from data import TestData
 
 class TestBurger:
 
@@ -9,35 +11,22 @@ class TestBurger:
         burger.set_buns(mock_bun)
         assert burger.bun == mock_bun
 
-    def test_add_ingredient(self):
-        mock_ingredient = Mock()
-        mock_ingredient.type = INGREDIENT_TYPE_SAUCE
-        mock_ingredient.name = "hot sauce"
-        mock_ingredient.price = 100
+    def test_add_ingredient(self, mock_sauce):
+        mock_ingredient = mock_sauce
         burger = Burger()
         burger.add_ingredient(mock_ingredient)
         assert burger.ingredients == [mock_ingredient]
 
-    def test_remove_ingredient(self):
-        mock_ingredient = Mock()
-        mock_ingredient.type = INGREDIENT_TYPE_SAUCE
-        mock_ingredient.name = "hot sauce"
-        mock_ingredient.price = 100
+    def test_remove_ingredient(self, mock_sauce):
+        mock_ingredient = mock_sauce
         burger = Burger()
         burger.add_ingredient(mock_ingredient)
         burger.remove_ingredient(0)
         assert burger.ingredients == []
 
-    def test_move_ingredient(self):
-        mock_ingredient1 = Mock()
-        mock_ingredient1.type = INGREDIENT_TYPE_SAUCE
-        mock_ingredient1.name = "Соус 1"
-        mock_ingredient1.price = 100
-
-        mock_ingredient2 = Mock()
-        mock_ingredient2.type = INGREDIENT_TYPE_FILLING
-        mock_ingredient2.name = "Начинка 2"
-        mock_ingredient2.price = 100
+    def test_move_ingredient(self, mock_sauce, mock_filling):
+        mock_ingredient1 = mock_sauce
+        mock_ingredient2 = mock_filling
 
         burger = Burger()
         burger.add_ingredient(mock_ingredient1)
@@ -66,22 +55,16 @@ class TestBurger:
         price = burger.get_price()
         assert price == 400
 
-    def test_get_receipt(self, mock_bun):
+    def test_get_receipt(self, mock_bun, mock_sauce, mock_filling):
         
-        mock_ingredient1 = Mock()
-        mock_ingredient1.get_type.return_value = INGREDIENT_TYPE_FILLING
-        mock_ingredient1.get_name.return_value = 'Начинка1'
-        mock_ingredient1.get_price.return_value = 100
-        mock_ingredient2 = Mock()
-        mock_ingredient2.get_type.return_value = INGREDIENT_TYPE_SAUCE
-        mock_ingredient2.get_name.return_value = 'Соус1'
-        mock_ingredient2.get_price.return_value = 150
+        mock_ingredient1 = mock_sauce
+        mock_ingredient2 = mock_filling
 
         burger = Burger()
         burger.set_buns(mock_bun)
         burger.add_ingredient(mock_ingredient1)
         burger.add_ingredient(mock_ingredient2)
         receipt = burger.get_receipt()
-        assert receipt == '(==== Булочка ====)\n= filling Начинка1 =\n= sauce Соус1 =\n(==== Булочка ====)\n\nPrice: 450'
+        assert receipt == f'(==== {TestData.name_bun} ====)\n= {str(mock_ingredient1.get_type()).lower()} {TestData.name_sauce} =\n= {str(mock_ingredient2.get_type()).lower()} {TestData.name_filling} =\n(==== {TestData.name_bun} ====)\n\nPrice: {TestData.burger_price}'
         
 
